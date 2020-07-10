99934bf75c768b6afaeeaebe74816c3bde38246d
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-07-10 15:32:17 -0400

    Setup Netlify build hooks

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -12,14 +12,14 @@ export default {
                 "NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.",
               sites: [
                 {
-                  buildHookId: "5ed3e87a7ee67e61febfde05",
+                  buildHookId: "5f08c098c17c0d01e2b62793",
                   title: "Sanity Studio",
                   name: "print-shop-studio",
                   apiId: "e55b37ce-c4a1-4ef2-bbd1-3a6274de887b",
                 },
                 {
-                  buildHookId: "5ed3e87a7668340283c1e017",
-                  title: "Landing pages Website",
+                  buildHookId: "5f08c12dc17c0d019bb627e8",
+                  title: "Print Shop Website",
                   name: "print-shop",
                   apiId: "7363ce59-f3aa-48ee-8a58-a2b2324d8d7d",
                 },
